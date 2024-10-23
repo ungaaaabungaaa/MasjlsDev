1912c6a3275776e25414fb3a2f91e87b35abc593
syed abdul muqeeth <syed_abdul_muqeeth@proton.me>
2024-10-23 06:17:50 +0530

    small error fixes

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -136,7 +136,7 @@ function HeroImage() {
       <HeroVideoDialog
         animationStyle="from-center"
         videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
-        thumbnailSrc="/appcoding.png"
+        thumbnailSrc="/Appcoding.png"
         thumbnailAlt="Hero Video"
         className="border rounded-lg shadow-lg max-w-screen-lg mt-16"
       />
