8370c87be5a3095645b3c5d392d9d876feda8d0f
syed abdul muqeeth <samx123786@gmail.com>
2025-07-12 19:10:13 +0530

    modified footer section

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -10,33 +10,13 @@ export default function Footer() {
         <a
           href="/"
           title={siteConfig.name}
-          className="relative mr-6 flex items-center space-x-2"
+          className="relative py-4 mr-6 flex items-center space-x-2"
         >
           <Icons.palmTreeLogo className="w-auto h-[40px]" />
           <span className="font-bold text-xl">{siteConfig.name}</span>
         </a>
 
-        <div className="grid md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 mt-8">
-          {siteConfig.footer.map((section, index) => (
-            <div key={index} className="mb-5">
-              <h2 className="font-semibold">{section.title}</h2>
-              <ul>
-                {section.links.map((link, linkIndex) => (
-                  <li key={linkIndex} className="my-2">
-                    <Link
-                      href={link.href}
-                      className="group inline-flex cursor-pointer items-center justify-start gap-1 text-muted-foreground duration-200 hover:text-foreground hover:opacity-90"
-                    >
-                      {link.icon && link.icon}
-                      {link.text}
-                      <ChevronRight className="h-4 w-4 translate-x-0 transform opacity-0 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:opacity-100" />
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
+
         <div className="max-w-6xl mx-auto border-t py-2 grid md:grid-cols-2 h-full justify-between w-full grid-cols-1 gap-1">
           <span className="text-sm tracking-tight text-foreground">
             Copyright © {new Date().getFullYear()}{" "}
